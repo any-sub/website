4a77919fbb835074853a3cbc9696268eae2c3ee9
Stefan <stefan@stefanpuia.co.uk>
2023-09-09 18:28:50 +0100

    using fake api

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,5 +1,5 @@
 export async function load() {
-	const url = `http://localhost:8080/docs/swagger.json`;
+	const url = `https://jsonplaceholder.typicode.com/todos/1`;
 	const response = await fetch(url);
 	return {
 		json: JSON.stringify(await response.json(), null, 4)
